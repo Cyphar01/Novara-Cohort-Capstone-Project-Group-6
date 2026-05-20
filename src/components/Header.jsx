958007cc60e7dc082e006e0429049e41e1fe9546
Olalekan <olalekanolayinka16@gmail.com>
2026-05-20 03:43:11 +0100

    I worked on the header and footer section

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="nav-container">
+        <img
+          src="https://res.cloudinary.com/dg3f5oyel/image/upload/v1778627241/Nav_Bar_2-removebg-preview_vzobjy.png"
+          alt="Planet Logo"
+          className="logo"
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
